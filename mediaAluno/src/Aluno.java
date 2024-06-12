73e80a6f088475ade13c8b5714f4bf85c90e1170
Samuel Alves Cardoso <wellsamuel@hotmail.com>
2024-06-12 20:24:58 -0300

    sac

--- mediaAluno/src/Aluno.java
+++ mediaAluno/src/Aluno.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Aluno {
-    int matricula;
-    String nome;
-    String curso;
-    List<Double> notas = new ArrayList<>();
+    private int matricula;
+    private String nome;
+    private String curso;
+    private List<Double> notas = new ArrayList<>();
 
     public Aluno(int matricula, String nome, String curso, List<Double> notas) {
         this.matricula = matricula;
@@ -18,9 +18,49 @@ public class Aluno {
 
     }
 
+    // public double calcularMedia( List<Double> notas, int quantidade)
+    // for (Double nota : notas )
+    // somaNota = somaNota + nota;
+    // return somaNota / quantidade ;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public List<Double> getNotas() {
+        return notas;
+    }
+
+    public void setNotas(List<Double> notas) {
+        this.notas = notas;
+    }
+
     public void calcularMedia(List<Double> notas, int qntNotas) {
         double somaNotas = 0.0;
-        for (int i = 0; i < qntNotas; i++) {
+
+        for (int i = 0; i < qntNotas; i++)
+
+        {
             somaNotas = somaNotas + notas.get(i);
         }
 
--- mediaAluno/src/App.java
+++ mediaAluno/src/App.java
@@ -1,29 +1,32 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner leitura = new Scanner(System.in);
         Aluno aluno = new Aluno();
+        ArrayList<Double> notas = new ArrayList<>();
 
         System.out.println("Qual o nome do aluno?");
-        aluno.nome = leitura.nextLine();
+        aluno.setNome(leitura.nextLine());
 
         System.out.println("Qual o curso do aluno?");
-        aluno.curso = leitura.nextLine();
+        aluno.setCurso(leitura.nextLine());
 
         System.out.println("Qual a matrícula do aluno?");
-        aluno.matricula = leitura.nextInt();
+        aluno.setMatricula(leitura.nextInt());
 
         System.out.println("Quantas notas o aluno tem?");
         int qntNotas = leitura.nextInt();
 
         for (int i = 1; i <= qntNotas; i++) {
             System.out.println("Qual a " + i + " nota?");
-            aluno.notas.add(leitura.nextDouble());
+            notas.add(leitura.nextDouble());
         }
-
+        aluno.setNotas(notas);
         aluno.mostrarDados(aluno);
 
-        aluno.calcularMedia(aluno.notas, qntNotas);
+        aluno.calcularMedia(notas, qntNotas);
     }
+
 }
