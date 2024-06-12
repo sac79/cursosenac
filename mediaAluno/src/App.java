0a993dfd0b7e60dfbdd2ab04cffffa2c6c704f89
Samuel Alves Cardoso <wellsamuel@hotmail.com>
2024-06-12 19:27:41 -0300

    Ajuste aula jaime

--- mediaAluno/src/App.java
+++ mediaAluno/src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leitura = new Scanner(System.in);
+        Aluno aluno = new Aluno();
+
+        System.out.println("Qual o nome do aluno?");
+        aluno.nome = leitura.nextLine();
+
+        System.out.println("Qual o curso do aluno?");
+        aluno.curso = leitura.nextLine();
+
+        System.out.println("Qual a matrícula do aluno?");
+        aluno.matricula = leitura.nextInt();
+
+        System.out.println("Quantas notas o aluno tem?");
+        int qntNotas = leitura.nextInt();
+
+        for (int i = 1; i <= qntNotas; i++) {
+            System.out.println("Qual a " + i + " nota?");
+            aluno.notas.add(leitura.nextDouble());
+        }
+
+        aluno.mostrarDados(aluno);
+
+        aluno.calcularMedia(aluno.notas, qntNotas);
     }
 }
